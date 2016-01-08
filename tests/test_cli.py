fe29ba2938224332a2a8cc6589049979789e3d82
fitnr <fitnr@fakeisthenewreal>
2016-01-08 10:59:36 -0500

    expand CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,23 +11,27 @@
 import unittest
 import subprocess
 import csv
+import sys
 try:
     import StringIO as io
 except ImportError:
     import io
 
 from pkg_resources import resource_filename
+from addfips import cli as addfips_cli
 
 
 class testcli(unittest.TestCase):
+
     def setUp(self):
         self.states = resource_filename('addfips', 'data/states.csv')
         self.counties = resource_filename('addfips', 'data/counties_2015.csv')
 
-    def testStateCli(self):
-        args = ['addfips', self.states, '-s', 'name']
-        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        self.st_args = ['addfips', self.states, '-s', 'name']
+        self.co_args = ['addfips', self.counties, '-c', 'name', '-s', 'statefp']
 
+    def testStateCliSubprocess(self):
+        p = subprocess.Popen(self.st_args, stdout=subprocess.PIPE)
         out, err = p.communicate()
 
         assert err is None
@@ -40,10 +44,8 @@ class testcli(unittest.TestCase):
         assert row['name'] == 'Alabama'
         assert row['fips'] == '01'
 
-    def testCountyCli(self):
-        args = ['addfips', self.counties, '-s', 'statefp', '-c', 'name']
-        p = subprocess.Popen(args, stdout=subprocess.PIPE)
-
+    def testCountyCliSubprocess(self):
+        p = subprocess.Popen(self.co_args, stdout=subprocess.PIPE)
         out, err = p.communicate()
 
         assert err is None
@@ -51,6 +53,39 @@ class testcli(unittest.TestCase):
         f = io.StringIO(out.decode('utf-8'))
 
         reader = csv.DictReader(f)
+        row = next(reader)
+
+        assert row['name'] == 'Autauga County'
+        assert row['fips'] == '01001'
+
+    def testCountyCliCall(self):
+        sys.argv = self.co_args
+        sys.stdout = io.StringIO()
+        addfips_cli.main()
+        sys.stdout.seek(0)
+        reader = csv.DictReader(sys.stdout)
+        row = next(reader)
+
+        assert row['name'] == 'Autauga County'
+        assert row['fips'] == '01001'
+
+    def testStateCliCall(self):
+        sys.argv = self.st_args
+        sys.stdout = io.StringIO()
+        addfips_cli.main()
+        sys.stdout.seek(0)
+        reader = csv.DictReader(sys.stdout)
+        row = next(reader)
+
+        assert row['name'] == 'Alabama'
+        assert row['fips'] == '01'
+
+    def testStateNameCliCall(self):
+        sys.argv = self.co_args[:-2] + ['--state-name', 'Alabama']
+        sys.stdout = io.StringIO()
+        addfips_cli.main()
+        sys.stdout.seek(0)
+        reader = csv.DictReader(sys.stdout)
         row = next(reader)
 
         assert row['name'] == 'Autauga County'
