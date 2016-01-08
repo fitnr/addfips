46b42947c4a2b9e8a1f437f530fd62cc5beb84e2
fitnr <fitnr@fakeisthenewreal>
2016-01-08 10:48:30 -0500

    refactor cli to be slightly more pythonic

--- addfips/cli.py
+++ addfips/cli.py
@@ -12,22 +12,22 @@ from signal import signal, SIGPIPE, SIG_DFL
 from . import __version__ as version
 from .addfips import AddFIPS
 
+
 def main():
     parser = argparse.ArgumentParser(description="Add FIPS codes to a CSV with state and/or county names")
-
     parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + version)
 
-    parser.add_argument('input', nargs='?', help='Input file. default: stdin', default='/dev/stdin')
-    parser.add_argument('-d', '--delimiter', metavar='CHAR', type=str, default=',', help='field delimiter default: ,', )
+    parser.add_argument('input', nargs='?', help='Input file. default: stdin')
+    parser.add_argument('-d', '--delimiter', metavar='CHAR', type=str, help='field delimiter default: ,')
 
     group = parser.add_mutually_exclusive_group(required=True)
-
     group.add_argument('-s', '--state-field', metavar='FIELD', type=str, help='Read state name or FIPS code from this field')
     group.add_argument('-n', '--state-name', metavar='NAME', type=str, help='Use this state for all rows')
 
-    parser.add_argument('-c', '--county-field', metavar='FIELD', type=str, default=None, help='default: None')
+    parser.add_argument('-c', '--county-field', metavar='FIELD', type=str, help='default: None')
     parser.add_argument('-v', '--vintage', type=str, help='2000 or current')
     parser.add_argument('--no-header', action='store_false', dest='header', help='Has no header now, interpret fields as integers')
+    parser.set_defaults(delimiter=',', input='/dev/stdin')
 
     args = parser.parse_args()
     af = AddFIPS(args.vintage)
@@ -37,13 +37,13 @@ def main():
     }
 
     if args.county_field:
-        func = 'add_county_fips'
+        func = af.add_county_fips
         kwargs["county_field"] = args.county_field
-
         if args.state_name:
             kwargs["state"] = args.state_name
+
     else:
-        func = 'add_state_fips'
+        func = af.add_state_fips
 
     with open(args.input, 'rt') as f:
         signal(SIGPIPE, SIG_DFL)
@@ -56,7 +56,7 @@ def main():
             writer.writeheader()
 
         else:
-            # Ignore the header, don't write a header.
+            # Don't read a header, don't write a header.
             kwargs['state_field'] = int(kwargs['state_field']) - 1
 
             if 'county_field' in kwargs:
@@ -65,10 +65,7 @@ def main():
             reader = csv.reader(f)
             writer = csv.writer(sys.stdout)
 
-        for row in reader:
-            new = getattr(af, func)(row, **kwargs)
-
-            writer.writerow(new)
+        writer.writerows(func(row, **kwargs) for row in reader)
 
 if __name__ == '__main__':
     main()
