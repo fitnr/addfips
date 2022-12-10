7da02883b2721879c2bf8a729f97345beb8a2bfa
fitnr <fitnr@fakeisthenewreal>
2022-12-09 19:39:15 -0500

    v0.4.1

--- src/addfips/__init__.py
+++ src/addfips/__init__.py
@@ -11,6 +11,6 @@ Add FIPS codes to lists and files that contain the names of US state and countie
 
 from .addfips import AddFIPS
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 __all__ = ['addfips']
